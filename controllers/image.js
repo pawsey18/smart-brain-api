63e794a4242b5866a28493c14fa5d51c539e0fb6
Nick Pawsey <pawsey18@gmail.com>
2020-12-03 15:55:00 -0400

    the snart brain api

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+
+const Clarifai = require('clarifai');
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+    apiKey: '859b48a8de624466aad422d95b377828'
+   });
+const handleApiCall = (req, res) =>{ 
+      app.models
+   .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+   .then(data => { 
+       res.json(data);
+   })
+   .catch(err => res.status(400).json('unable to work with api'))
+
+const handleImage = (req, res, db) => { 
+    const { id} = req.body;
+     db('users').where('id', '=', id)
+     .increment('entries', 1)
+     .returning('entries')
+     .then(entries => { 
+         res.json(entries[0]);
+     })
+     .catch(err => res.status(400).json('unable to get entires'))
+}
+
+module.exports = { 
+    handleImage,
+    handleApiCall
+}};
---
+++ server.js
@@ -0,0 +1,65 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors());
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+
+const db = knex({ 
+    client: 'postgres',
+    connection: { 
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : '22323',
+        port : '5432',
+        database : 'smart-brain'
+    }
+});
+
+
+app.get('/', (req, res) => { 
+   res.send("success");
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
+
+app.post('/register', (req, res) => {register.handleRegister(req, res,db,bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} )
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+/* // storing hash in your password db
+bcrypt.hash('bacon', null, null, function(err,hash) { 
+
+});
+
+// load hash from your password db
+bcrypt.compare('bacon', hash, function(err,res) { 
+// res == true
+});
+
+bcrypt.compare('pussy', hash, function(err,res) { 
+// res = false
+}); */
+
+app.listen(3000, () => { 
+    console.log("i guess it worked after all")
+} )
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
