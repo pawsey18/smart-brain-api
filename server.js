0140e47c1d8a534a669f35cdee1f2b1103b10014
Nick Pawsey <pawsey18@gmail.com>
2020-12-07 19:35:26 -0400

    sdfg

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 
-const app = express();
-app.use(bodyParser.json())
-app.use(cors());
+
+
 const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile.js');
 const image = require('./controllers/image.js');
-
 
 const db = knex({ 
     client: 'pg',
@@ -22,15 +20,16 @@ const db = knex({
     }
 });
 
-app.get('/', (req, res) => { 
-   res.send("success");
-})
 
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors());
+
+app.get('/', (req, res) => { res.send("success");})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
-
 app.post('/register', (req, res) => {register.handleRegister(req, res,db,bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)} )
-
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
