9b6033953234a4a7fe2fbbeb4612b04e78bfa663
Nick Pawsey <pawsey18@gmail.com>
2020-12-08 09:21:43 -0400

    nodemode fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express= require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
